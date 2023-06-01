c57e323bd216dfedceb40c61620e5cecd087b930
Micaela-ux <ruiz.mica@hotmail.com>
2023-06-01 20:42:42 -0300

    feat: delete links

--- clase_10/src/App.vue
+++ clase_10/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Estamos probando Vue2 vía cli"/>
   </div>
 </template>
 
